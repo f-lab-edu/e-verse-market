cc6a9dbe59b069d8561bae662083372f2b33a6a8
Seon1134 <jseon382@gmail.com>
2023-10-23 20:16:19 +0900

    build: MySQL, JPA, and Lombok Dependencies

---
+++ src/main/kotlin/com/seon/eversemarket/EVerseMarketApplication.kt
@@ -0,0 +1,12 @@
+package com.seon.eversemarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EVerseMarketApplication
+
+fun main(args: Array<String>) {
+	runApplication<EVerseMarketApplication>(*args)
+
+}
---
+++ src/test/kotlin/com/seon/eversemarket/EVerseMarketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.seon.eversemarket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EVerseMarketApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
